787e30a168befc4847a585ad67973b0371ff325f
ManonLudt <manon.ludt99@gmail.com>
2025-02-15 00:11:18 +0100

    Update SQL Code mit Daten.sql

--- Implementierung/SQL Code/SQL Code mit Daten.sql
+++ Implementierung/SQL Code/SQL Code mit Daten.sql
@@ -33,6 +33,17 @@ IF OBJECT_ID('Reservierung') IS NOT NULL
     DROP TABLE Reservierung;
 GO
 
+IF OBJECT_ID('Benutzer') IS NOT NULL
+    DROP TABLE Benutzer;
+GO
+
+create table Benutzer
+(
+	ID_Benutzer int primary key IDENTITY(1,1),
+	Benutzername varchar(100),
+	Passwort int,
+);
+
 create table Besucher
 (
 	ID_Besucher int primary key IDENTITY(1,1),
@@ -51,7 +62,7 @@ create table Status
 
 create table StatusBesucher
 (
-	ID_Besucher INT,
+    ID_Besucher INT,
     ID_Status INT,
     PRIMARY KEY (ID_Besucher, ID_Status),
     FOREIGN KEY (ID_Besucher) REFERENCES Besucher(ID_Besucher),
@@ -62,7 +73,7 @@ create table Schwimmbad
 (
 	ID_Schwimmbad int primary key identity(1,1),
 	Name varchar(100),
-	Straﬂe varchar(100),
+	Stra√üe varchar(100),
 	Ort varchar(100),
 	PLZ int,
 	Auslastung int,
@@ -98,13 +109,13 @@ SET IDENTITY_INSERT Besucher ON;
 INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(1,'Lukas', 'Schneider', 17)
 INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(2,'Emma', 'Weber', 23)
 INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(3,'Noah', 'Fischer', 20)
-INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(4,'Mia', 'M¸ller', 41)
+INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(4,'Mia', 'M√ºller', 41)
 INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(5,'Ben', 'Schmidt', 16)
 INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde) VALUES(6,'Sophie', 'Braun', 22)
 SET IDENTITY_INSERT Besucher OFF; 
 
 SET IDENTITY_INSERT Status ON;
-INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(1,'Sch¸ler', 0.90)
+INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(1,'Sch√ºler', 0.90)
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(2,'Student', 0.95)
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(3,'Kind', 0.75)
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(4,'Erwachsen', 1)
@@ -119,10 +130,10 @@ INSERT INTO StatusBesucher VALUES
 (6, 3)
 
 SET IDENTITY_INSERT Schwimmbad ON;
-INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Straﬂe, Ort, PLZ, Auslastung) VALUES(1,'Freibad Sonnensee', 'Seestraﬂe 4', 'M¸nchen', 80331, 113)
-INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Straﬂe, Ort, PLZ, Auslastung) VALUES(2,'Hallenschwimmbad Westend', 'Westendstraﬂe 56', 'Frankfurt', 60322, 82)
-INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Straﬂe, Ort, PLZ, Auslastung) VALUES(3,'Erlebnisbad Blaues Wasser', 'Wasserstraﬂe 7', 'Hamburg', 20095, 136)
-INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Straﬂe, Ort, PLZ, Auslastung) VALUES(4,'Sportbad Nordpark', 'Nordparkstraﬂe 25', 'Kˆln', 50667, 78)
+INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Stra√üe, Ort, PLZ, Auslastung) VALUES(1,'Freibad Sonnensee', 'Seestra√üe 4', 'M√ºnchen', 80331, 113)
+INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Stra√üe, Ort, PLZ, Auslastung) VALUES(2,'Hallenschwimmbad Westend', 'Westendstra√üe 56', 'Frankfurt', 60322, 82)
+INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Stra√üe, Ort, PLZ, Auslastung) VALUES(3,'Erlebnisbad Blaues Wasser', 'Wasserstra√üe 7', 'Hamburg', 20095, 136)
+INSERT INTO Schwimmbad(ID_Schwimmbad, Name, Stra√üe, Ort, PLZ, Auslastung) VALUES(4,'Sportbad Nordpark', 'Nordparkstra√üe 25', 'K√∂ln', 50667, 78)
 SET IDENTITY_INSERT Schwimmbad OFF;
 
 INSERT INTO SchwimmbadBesucher VALUES
