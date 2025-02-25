aea920905f06849cbed634cd998f632d10d579aa
ManonLudt <manon.ludt99@gmail.com>
2025-02-25 14:59:53 +0100

    Add files via upload

--- Implementierung/SQL Code/SQL Code mit Daten.sql
+++ Implementierung/SQL Code/SQL Code mit Daten.sql
@@ -21,6 +21,10 @@ IF OBJECT_ID('Benutzer') IS NOT NULL
     DROP TABLE Benutzer;
 GO
 
+IF OBJECT_ID('Reservierung') IS NOT NULL
+    DROP TABLE Reservierung;
+GO
+
 create table Benutzer
 (
 	ID_Benutzer int primary key IDENTITY(1,1),
@@ -80,23 +84,23 @@ INSERT INTO Besucher(ID_Besucher, Vorname, Nachname, AlterKunde, Status) VALUES(
 SET IDENTITY_INSERT Besucher OFF;
 
 SET IDENTITY_INSERT Ticket ON;
-INSERT INTO Ticket( Bezeichnung, Preis, Anzahl) VALUES('Standartticket', 4, 154)
-INSERT INTO Ticket( Bezeichnung, Preis, Anzahl) VALUES('Tagesticket', 5, 123)
-INSERT INTO Ticket( Bezeichnung, Preis, Anzahl) VALUES('Kinderticket', 3, 96)
-INSERT INTO Ticket( Bezeichnung, Preis, Anzahl) VALUES('Familienticket', 7.5, 109)
-INSERT INTO Ticket( Bezeichnung, Preis, Anzahl) VALUES('Gruppenticket', 10, 75)
-SET IDENTITY_INSERT Ticket ON;
+INSERT INTO Ticket(ID_Ticket, Bezeichnung, Preis, Anzahl) VALUES(1, 'Standartticket', 4, 154)
+INSERT INTO Ticket(ID_Ticket, Bezeichnung, Preis, Anzahl) VALUES(2, 'Tagesticket', 5, 123)
+INSERT INTO Ticket(ID_Ticket, Bezeichnung, Preis, Anzahl) VALUES(3, 'Kinderticket', 3, 96)
+INSERT INTO Ticket(ID_Ticket, Bezeichnung, Preis, Anzahl) VALUES(4, 'Familienticket', 7.5, 109)
+INSERT INTO Ticket(ID_Ticket, Bezeichnung, Preis, Anzahl) VALUES(5, 'Gruppenticket', 10, 75)
+SET IDENTITY_INSERT Ticket OFF;
 
 SET IDENTITY_INSERT Status ON;
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(1,'Schüler', 0.90)
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(2,'Student', 0.95)
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(3,'Kind', 0.75)
 INSERT INTO Status(ID_Status, Bezeichnung, Rabatt) VALUES(4,'Erwachsen', 1)
-SET IDENTITY_INSERT Status ON;
+SET IDENTITY_INSERT Status OFF;
 
 SET IDENTITY_INSERT Reservierung ON;
 INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(1,'Lukas', 'Schneider', 'Schüler', 'Tagesticket', 10, 1)
-INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(3,'Noah', 'Fischer', 'Student', 'Standartticket', 5, 0)
-INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(4,'Mia', 'Müller', '', 'Familienticket', 10, 0)
-INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(6,'Sophie', 'Braun', '', 'Standartticket', 10, 1)
+INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(2,'Noah', 'Fischer', 'Student', 'Standartticket', 5, 0)
+INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(3,'Mia', 'Müller', '', 'Familienticket', 10, 0)
+INSERT INTO Reservierung(ID_Reservierung, Vorname, Nachname, Status, Ticket, Rabatt, Anwesend) VALUES(4,'Sophie', 'Braun', '', 'Standartticket', 10, 1)
 SET IDENTITY_INSERT Reservierung OFF;
